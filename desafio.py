5ab2b2bfb51fcf6c1676c5de8466ea3c5b977d52
Guilherme Nunes <112637353+Nunes27@users.noreply.github.com>
2023-06-05 11:42:58 -0300

    .

---
+++ desafio.py
@@ -0,0 +1,11 @@
+dia = input('Qual dia você nasceu? ')
+mes = input('Qual mes você nasceu? ')
+ano = input('Qual ano você nasceu? ')
+
+print('Você nasceu no dia', dia, 'de', mes, 'de', ano)
+
+nm1 = input('Primeiro numero: ')
+nm2 = input('Segundo numero: ')
+
+print = ('A some é ', nm1 + nm2)
+

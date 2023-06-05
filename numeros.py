bad14f40edefaae9a7f62cf6fdf841664b7d6601
Guilherme Nunes <112637353+Nunes27@users.noreply.github.com>
2023-06-05 13:45:48 -0300

    .

---
+++ numeros.py
@@ -0,0 +1,11 @@
+#int - inteiros
+#... -3, -2, -1,0,1,2,3...
+
+#float -3,-3.4
+
+1.0#float
+1.
+
+#complex
+1+2j #complex
+
---
+++ variaveis.py
@@ -0,0 +1,9 @@
+x = 5 # x recebe o valor 5
+y = 3.5
+z = 1+2j
+
+w = "ana"
+w = 'ana'
+
+#A-z, 0-9 e _
+

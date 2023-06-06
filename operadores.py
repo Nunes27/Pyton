4bb4dd68cef864edf2867c092fc934c01458771a
Guilherme Nunes <112637353+Nunes27@users.noreply.github.com>
2023-06-06 13:46:34 -0300

    Create operadores.py

---
+++ operadores.py
@@ -0,0 +1,22 @@
+"""
+Operadores Aritmeticos
+
+"""
+
+nome = 8
+x = 5
+y = 3
+
+# Operador de atribuição =
+
+"""
+operador da soma +
+Operador da subtração -
+Operador da multip  *
+Operador Divisão /
+
+"""
+x = 3
+y = 5
+z = x - y
+print(z)

3b2255acac6d60d193914837973dd65284418f88
Guilherme Nunes <112637353+Nunes27@users.noreply.github.com>
2023-06-06 10:17:07 -0300

    Create tipos_de_dados.py

---
+++ tipos_de_dados.py
@@ -0,0 +1,22 @@
+"""
+Variavel e os tipos de dados
+
+tipos de dados:
+
+tipos de texto: str
+tipos numericos: int, float e o complex
+
+tipos de sequencia: lits, tuple, range
+tipo de mapeamento: dict
+tipos Set: set, frozenset
+
+tipos de booleano: bool
+Tipos binarios: bytes, bytearray, momoryview
+
+print()
+type()
+
+#casting float, int, str
+
+"""
+

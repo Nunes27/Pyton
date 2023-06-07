2745ea80d6409baf8e9c69e2a7c773f796eed9ee
Guilherme Nunes <112637353+Nunes27@users.noreply.github.com>
2023-06-07 09:14:03 -0300

    Update boolean.py

--- .vscode/boolean.py
+++ .vscode/boolean.py
@@ -16,3 +16,21 @@ or (ou)
 not (negação)
 
 """
+
+"""
+Tabela Verdade
+
+Utilizando o "E"
+
+Verdadeiro e Verdadeiro - True and True = True
+Verdadeiro e Falso - True and False = False
+Falso e Verdadeiro - False and True = False
+Falso e falso - False and False = False
+
+Utilizando o "ou" (or)
+
+Verdadeiro ou Verdadeiro - True or True = True
+Verdadeiro ou Falso - True or False = True
+Falso ou Verdadeiro - False or True = True
+Falso ou falso - False or False = False
+"""

dbe46f705125b29a6519b2b1604d5b30147fcbc0
Guilherme Nunes <112637353+Nunes27@users.noreply.github.com>
2023-06-07 09:42:09 -0300

    Create input.py

---
+++ .vscode/input.py
@@ -0,0 +1,11 @@
+"""
+Função imput - função para receber dados de usuarios
+
+"""
+print()
+nome = input('Qual é o seu nome? ')
+idade = input('Qual a sua idade? ')
+
+print('Seu nome é: {0} e sua idade é {1} anos'.format(nome, idade))
+
+print(f'Seu nome é: {nome} e sua idade é {idade}')

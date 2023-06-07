9241544f85a79d87fbf9022097edbd53f0858a74
Guilherme Nunes <112637353+Nunes27@users.noreply.github.com>
2023-06-07 08:00:30 -0300

    Create boolean.py

---
+++ .vscode/boolean.py
@@ -0,0 +1,18 @@
+"""
+Boolean
+
+0 e 1, verdadeiro e falso.
+
+possui esse nome em homenagem a George Boole
+
+"""
+# tipo boolean : True e false
+
+"""
+Operadores Logicos
+
+and (e)
+or (ou)
+not (negação)
+
+"""

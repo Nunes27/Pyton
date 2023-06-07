7f1aeaad41fbf4315f754fe1f3d0370bd7283d77
Guilherme Nunes <112637353+Nunes27@users.noreply.github.com>
2023-06-07 10:26:43 -0300

    .

---
+++ .vscode/associacao.py
@@ -0,0 +1,16 @@
+""""
+Operadores de associação
+
+nome = 'marcos'
+
+operador in - x in y
+operador not in - x not in y
+
+
+"""
+
+x = 'pernambuco'
+
+y = 'per'
+
+print(y in x)
---
+++ .vscode/comparacao.py
@@ -0,0 +1,19 @@
+"""
+Operadores de Comparação
+
+operador de igualdade ==
+Operador de Diferença !=
+operador maior que >
+operador menor que <
+operador maior que ou igual a >=
+operador menor que ou igual a <=
+"""
+
+# Operador de atribuição
+
+x = 3
+y = 5
+z = 3.0
+w = 8
+
+print(x == y)

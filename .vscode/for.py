cffd803887ab6e6eacb9a859d32d8656b87c0cd8
Guilherme Nunes <112637353+Nunes27@users.noreply.github.com>
2023-06-12 15:21:18 -0300

    .

---
+++ .vscode/for.py
@@ -0,0 +1,16 @@
+"""
+
+Estruturas de repetição
+While
+for
+do while*
+
+"""
+"""
+s = 'pernanbuco'
+
+for x in s:
+    print(x)"""
+
+for x in range(6): #range(6) significa que começamos do 0 até termos 6 digitos
+    print(x)
---
+++ .vscode/while.py
@@ -0,0 +1,17 @@
+"""
+
+Estruturas de Repetições - loops
+
+while
+for
+do while
+"""
+
+a = 0
+while a <= 5:
+    print(a <= 5)
+    print(a)
+    a = a + 1
+else:
+    print(f"a não é menor ou iguala 5. Valor de a : {a}")
+    

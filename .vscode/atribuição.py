683f6e77ed4e1c5281fce6984a22e4b9c4c8f53a
Guilherme Nunes <112637353+Nunes27@users.noreply.github.com>
2023-06-27 15:53:24 -0300

    ;

--- .vscode/atribuição.py
+++ .vscode/atribuição.py
@@ -1,4 +1,11 @@
 x = 1
-print('Valor de x anteriormente:',x)
-x = 1 + 1 
-print('Valor de x apos a reescrita:',x)
+#print('Valor de x anteriormente:',x)
+x += 1 
+#print('Valor de x apos a reescrita:',x)
+x -= 1
+x *= 3
+x **= 2
+x /= 9
+x %= 9 
+x //= 5
+print(x)
